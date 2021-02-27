dd816a08b48ee526d06166b1c704b0e13c3d3641
Shubham Kapoor <shubhamkapoor9778@gmail.com>
2021-02-27 03:52:18 -0800

    Draws a basic white-grey chess board (without pieces)

--- ChessBot/ChessMain.py
+++ ChessBot/ChessMain.py
@@ -7,6 +7,11 @@ also be responsible for determining the valid moves at the current state. It wil
 import pygame
 from ChessBot import ChessEngine
 
+'''
+Initialization of important properties and libraries
+'''
+pygame.init()
+
 WIDTH = HEIGHT = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
@@ -21,5 +26,54 @@ def loadImages():
     pieces = ["wP", "wR", "wN", "wB", "wK", "wQ", "bP", "bR", "bN", "bB", "bK", "bQ"]
 
     for piece in pieces:
-        IMAGES[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
-        
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load("resources/images/chess_pieces/" + piece + ".png"),
+                                               (SQ_SIZE, SQ_SIZE))
+
+
+
+'''
+The main driver for the code, will handle user input and update the game accordingly
+'''
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color("white"))
+    game_state = ChessEngine.GameState()
+    loadImages() #should only be done once
+    running = True
+
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+        drawGameState(screen, game_state)
+        clock.tick(MAX_FPS)
+        pygame.display.flip()
+
+'''
+Responsible for drawing the graphics for the current game state
+'''
+
+def drawGameState(screen, gs):
+    drawSquares(screen)
+    drawPieces(screen, gs.board)
+
+'''
+Draws the squares on the board
+'''
+def drawSquares(screen):
+    colors = [pygame.Color("white"), pygame.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            pygame.draw.rect(screen, color, pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+'''
+Draws the pieces on the board
+'''
+def drawPieces(screen, board):
+    pass
+
+if __name__ == "__main__":
+    main()
